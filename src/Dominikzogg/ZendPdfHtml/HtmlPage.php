82c67da1c00101c38bbef39df88abc5c41082df2
Dominik Zogg <dominik.zogg@gmail.com>
2013-07-19 10:21:49 +0200

    prepare dependencies

--- src/Dominikzogg/ZendPdfHtml/HtmlPage.php
+++ src/Dominikzogg/ZendPdfHtml/HtmlPage.php
@@ -12,15 +12,59 @@ class HtmlPage
      */
     protected $page;
 
+    /**
+     * @var Html
+     */
+    protected $parser;
+
     public function __construct(Page $page)
     {
+        $this->setPage($page);
+    }
+
+    /**
+     * @param Page $page
+     * @return HtmlPage
+     */
+    public function setPage(Page $page)
+    {
         $this->page = $page;
+        return $this;
+    }
+
+    /**
+     * @return Page
+     */
+    protected function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * @param Html $parser
+     * @return HtmlPage
+     */
+    public function setParser(Html $parser)
+    {
+        $this->parser = $parser;
+        return $this;
+    }
+
+    /**
+     * @return Html
+     */
+    protected function getParser()
+    {
+        if(is_null($this->parser)) {
+            $this->parser = new Html();
+        }
+
+        return $this->parser;
     }
 
     public function drawHtml($html, $x, $y, $w = 0)
     {
-        $parser = new Html();
-        $parts = $parser->parse($html);
-        print_r($parts);
+        $elements = $this->getParser()->parse($html);
+        print_r($elements);
     }
 }
