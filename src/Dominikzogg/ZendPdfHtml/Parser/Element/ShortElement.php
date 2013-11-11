e71087548031f36de5cd1321dd85df32cd6346a0
Dominik Zogg <dominik.zogg@gmail.com>
2013-11-11 11:18:51 +0100

    fix name

--- src/Dominikzogg/ZendPdfHtml/Parser/Element/ShortElement.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Dominikzogg\ZendPdfHtml\Parser\Element;
-
-class ShortElement extends ControllElement
-{
-
-}
--- src/Dominikzogg/ZendPdfHtml/Parser/Element/StartElement.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Dominikzogg\ZendPdfHtml\Parser\Element;
-
-class StartElement extends ControllElement
-{
-
-}
